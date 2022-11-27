2247c71478059e832de17f9081e920be2b39f9da
Emmyabs <abujemilu555@gmail.com>
2022-11-27 12:47:39 +0100

    Creating a calculator with html ,css and javascript

---
+++ calculator.js
@@ -0,0 +1,7 @@
+function valuecal(result){
+    calculatorForm.evalresult.value = calculatorForm.evalresult.value + result;
+}
+function calcNumbers(result){
+    form.displayResult.value=form.displayResult.value+result;
+ 
+}
